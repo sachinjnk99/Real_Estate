d680fccffdddad3ae3c4c4463f748d2867e0ade2
sachinjnk99 <np03cs4a210041@heraldcollege.edu.np>
2024-05-18 00:24:53 +0545

    add popup dialog box

--- user/myproperty1.php
+++ user/myproperty1.php
@@ -102,7 +102,7 @@ if(!isset($_SESSION['u_email']))
                       <input type="submit" class="btn btn-success mt-5" name="insert" value="Update Property">
                                 </a>
 
-                    <a href="property-delete.php?pid=<?php echo $row['pid'];?>">
+                    <a href="property-delete.php?pid=<?php echo $row['pid'];?>" onclick='return checkdel()'>
                       <div class="float-right" >
                       <input type="submit" class="btn btn-danger mt-5" name="insert" value="Delect Property">
                                 </div></a>
@@ -136,6 +136,12 @@ if(!isset($_SESSION['u_email']))
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
+
+        <script>
+  function checkdel(){
+    return confirm('Are you sure want to delete record ?');
+  }
+</script>
 
 </body>
 
